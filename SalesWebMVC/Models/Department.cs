4a0dfe2a65b95b0814186c6dfdea66017a4b897d
Lucas <lucas_ssilva@outlook.com>
2020-01-14 22:20:51 -0300

    criação das outras entities e segunda migration

--- SalesWebMVC/Models/Department.cs
+++ SalesWebMVC/Models/Department.cs
@@ -9,6 +9,26 @@ namespace SalesWebMVC.Models
     {
         public int Id { get; set; }
         public String Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+
+        public Department() { }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void AddSeller(Seller sl)
+        {
+            Sellers.Add(sl);
+        }
+
+        public double TotalSalles(DateTime initial, DateTime final)
+        {
+            return Sellers.Sum(x => x.TotalSales(initial, final));
+        }
+
 
     }
 }
