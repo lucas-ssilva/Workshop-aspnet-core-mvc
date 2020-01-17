fa2a8b1f0a141fe09c72df9dac3590a94f657614
Lucas <lucas_ssilva@outlook.com>
2020-01-16 22:57:33 -0300

    Criando a função create seller, mexendo no html

--- SalesWebMVC/Controllers/SellersController.cs
+++ SalesWebMVC/Controllers/SellersController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SalesWebMVC.Models;
 using SalesWebMVC.Services;
 
 namespace SalesWebMVC.Controllers
@@ -20,6 +21,17 @@ namespace SalesWebMVC.Controllers
         {
             var list = _sellerService.FindAll();
             return View(list);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost] //indica que é post e não get , post pode mexer e atualizar banco 
+        [ValidateAntiForgeryToken] // segurança , previne que usem sua sesão aberta para enviar dados 
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller); //insere no banco
+            return RedirectToAction(nameof(Index)); //redireciona o usuario para a tela index 
         }
     }
 }
--- SalesWebMVC/Services/SellerService.cs
+++ SalesWebMVC/Services/SellerService.cs
@@ -19,5 +19,11 @@ namespace SalesWebMVC.Services
         {
             return _context.Seller.ToList(); // acessa as base e converte a tabela selecionada em lista . sincrona - a aplicação para ate a lista voltar 
         }
+
+        public void Insert(Seller obj)
+        {
+            _context.Add(obj); //insere objeto no banco de dados 
+            _context.SaveChanges(); // salva alteração 
+        }
     }
 }
