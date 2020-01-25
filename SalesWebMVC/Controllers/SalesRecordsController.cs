8806913f895ded594d95db5ded5485fafce1c849
Lucas <lucas_ssilva@outlook.com>
2020-01-24 23:34:59 -0300

    implementando a busca simples, criando controller de vendas

--- SalesWebMVC/Controllers/SalesRecordsController.cs
+++ SalesWebMVC/Controllers/SalesRecordsController.cs
@@ -3,18 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SalesWebMVC.Services;
 
 namespace SalesWebMVC.Controllers
 {
     public class SalesRecordsController : Controller
     {
+        private readonly SalesRecordService _salesRecordService;
+
+        public SalesRecordsController(SalesRecordService salesRecordService)
+        {
+            _salesRecordService = salesRecordService;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
-        public IActionResult SimpleSearch()
+        public async Task<IActionResult> SimpleSearch(DateTime? minDate, DateTime? MaxDate)
         {
-            return View();
+            if (!minDate.HasValue)
+            {
+                minDate = new DateTime(DateTime.Now.Year, 1, 1);
+            }
+            if (!MaxDate.HasValue)
+            {
+                MaxDate = DateTime.Now;
+            }
+            ViewData["minDate"] = minDate.Value.ToString("yyyy-MM-dd");
+            ViewData["MaxDate"] = MaxDate.Value.ToString("yyyy-MM-dd");
+
+            var result = await _salesRecordService.FindByDateAsync(minDate, MaxDate);
+            return View(result);
         }
         public IActionResult GroupingSearch()
         {
--- SalesWebMVC/Models/SalesRecord.cs
+++ SalesWebMVC/Models/SalesRecord.cs
@@ -3,16 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using SalesWebMVC.Models.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace SalesWebMVC.Models
 {
     public class SalesRecord
     {
+        [Display(Name = "ID")]
         public int Id { get; set; }
+        [Display(Name = "Data")]
         public DateTime Date { get; set; }
+        [Display(Name = "Valor")]
         public double Amount { get; set; }
+        [Display(Name = "Status")]
         public SaleStatus Status { get; set; }
-
+        [Display(Name = "Vendedor")]
         public Seller Seller { get; set; }
 
         public SalesRecord() { }
